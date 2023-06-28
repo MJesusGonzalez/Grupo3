5004ea44912d52a4466dc604b924fcb5f2ecd025
Joxxx69 <santiago.gualotuna@hotmail.com>
2023-06-27 22:49:32 -0500

    cambios principales en app.js

--- XP/Coding/app.js
+++ XP/Coding/app.js
@@ -17,12 +17,24 @@ const main = async() => {
         switch (opcion) {
             case '1':   // Crear tarea
                 const desc = await leerInput('Descripcion: ');
+                listaTareas.crearTarea(desc);
                 break;
             case '2':  // Listar todas las tareas
+                listaTareas.listadoCompleto();
                 break;
             case '3': // marcar como completadas y pendientes 
+                const ids= await mostrarListadoCompletado(listaTareas.getListado);
+                listadoTareas.toggleCompletadas(ids);
                 break;
             case '4': // Borrar tareas
+                const id = await listadoTareasBorrar(listaTareas.getListado);
+                if (id !== '0') {
+                    const confirmar = await confirmarPregunta('Estas Seguro ?');
+                    if (confirmar) {
+                        listaTareas.borrarTarea(id);
+                        console.log('  Tarea borrada');
+                    }
+                }
                 break;
             
         }
