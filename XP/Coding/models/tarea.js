5b31e07d7085c9f9064f0e4bb596b65552e6fb29
RonGonzalez01 <ronnie.gon12@gmail.com>
2023-06-27 22:35:26 -0500

    Clase Tarea

---
+++ XP/Coding/models/tarea.js
@@ -0,0 +1,36 @@
+const {v4: uuidv4} = require('uuid');
+const colors = require('colors');
+
+class Tarea{
+    static fromJson({id,descripcion,completado}){
+        const tarea = new Tarea(descripcion);
+        console.log('tareas: ',tarea.getDescripcion)
+        tarea.#id = id;
+        tarea.#completado = completado;
+        tarea.#descripcion = descripcion;
+        return tarea
+    }
+    #id ='';
+    #descripcion='';
+    #completado=null;
+    constructor(descripcion){
+        this.#descripcion = descripcion;
+        this.#id = uuidv4();
+        this.#completado = false;
+    }
+    get getDescripcion(){
+        return this.#descripcion;
+    }
+    get getCompletado(){
+        return this.#completado;
+    }
+    get getId(){
+        return this.#id;
+    }
+    set setCompletado(completado){
+        this.#completado = completado;
+    }    
+}
+
+module.exports = Tarea;
+
